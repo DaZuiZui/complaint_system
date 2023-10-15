9b695aa8a6b711c71a5a7fd34dbd276a4d603ae5
Jasper Han <18945342298@163.com>
2023-10-15 17:19:57 +0800

    task板块（查看所有信息） hln 2023-10-15 17:19

--- web-server/src/main/java/com/example/complaint_system/domain/bo/TaskAndUserBo.java
+++ web-server/src/main/java/com/example/complaint_system/domain/bo/TaskAndUserBo.java
@@ -1,16 +1,23 @@
 package com.example.complaint_system.domain.bo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class TaskAndUserBo implements Serializable {
     private String token;
     private String username;    //用户名
     private String context;    //投诉内容
+    private Long id;
+    private Date createTime;    //创建时间
+    private Date updateTime;    //修改时间
 
-    public TaskAndUserBo(String token, String username, String context) {
+    public TaskAndUserBo(String token, String username, String context, Long id, Date createTime, Date updateTime) {
         this.token = token;
         this.username = username;
         this.context = context;
+        this.id = id;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
     }
 
     public TaskAndUserBo() {
@@ -40,12 +47,39 @@ public class TaskAndUserBo implements Serializable {
         this.context = context;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
     @Override
     public String toString() {
         return "TaskAndUserBo{" +
                 "token='" + token + '\'' +
                 ", username='" + username + '\'' +
                 ", context='" + context + '\'' +
+                ", id=" + id +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
                 '}';
     }
 }
