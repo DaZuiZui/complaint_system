864f3c7b38cb238712c93deadf9ae7d668db344f
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-10-16 18:53:18 +0800

    清理ThreadLocal 防止内存泄漏

---
+++ web-server/src/main/java/com/example/complaint_system/aop/SystemAop.java
@@ -0,0 +1,15 @@
+package com.example.complaint_system.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface SystemAop {
+    /**
+     * @auhtor bryan yang
+     * 清理ThreadLocal 防止内存泄漏
+     * @param joinpoint
+     * @throws Exception
+     */
+    public void removeAllThreadLocal(JoinPoint joinpoint) throws Exception;
+}
